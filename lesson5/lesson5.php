ae647a2b754cbf9be7691acad0aed578844005a9
Karolis-Kvatavicius <39552171+Karolis-Kvatavicius@users.noreply.github.com>
2018-08-07 17:00:51 +0300

    Corrected sorting surnames from shortest to longest

--- lesson5/lesson5.php
+++ lesson5/lesson5.php
@@ -1,16 +1,16 @@
 <?php
 print '<pre>';
+print '<h3>Testas</h3>';
 $a = [1,2,3,4];
 
+//& leidzia modifikuoti masyva prieidamas prie jo elementu per reference.
 foreach ($a as &$value) {
     print '<br>'. $value. ' - ' .$a[3]; 
 }
 
-//& leidzia modifikuoti masyva prieidamas prie jo elementu per reference.
 //Referencams reikia daryt unset(), kitaip jis niekur nedingsta, ir sukelia 
 //nepageidaujamus rezultatus toliau einanciame kode.
 unset($value);
-
 
 print '<br>';
 foreach ($a as $value) {
@@ -18,7 +18,7 @@ foreach ($a as $value) {
 }
 print '<br><br>';
 
-//funkcija, kuri generuotu vardas(5-7) pavarde(7-10)
+//funkcija, kuri generuotu vardas(5-7 raides) pavarde(7-10 raidziu)
 function generateNameSurname() {
     $nameLength = rand(5, 7);
     $surnameLength = rand(7, 12);
@@ -38,7 +38,8 @@ function generateNameSurname() {
     return ucfirst($randomName). ' - ' .ucfirst($randomSurname);
 }
 
-//masyvas uzpildytas generateNameSurname() funkcija is 30 elementu.
+//Masyvas uzpildytas generateNameSurname() funkcija is 30 elementu.
+print '<h3>Masyvas uzpildytas generateNameSurname() funkcija</h3>';
 $arrayOfPeople = [];
 
 for ($i=0; $i < 30; $i++) { 
@@ -47,23 +48,19 @@ for ($i=0; $i < 30; $i++) {
 print_r($arrayOfPeople);
 
 //Paliekamos tik pavardes
+print '<h3>Paliekamos tik pavardes</h3>';
 foreach ($arrayOfPeople as &$value) {
     $value = str_replace('- ', '', substr($value, strpos($value, '-'), strlen($value)));
 }
 unset($value);
+print_r($arrayOfPeople);
+
 
 //Masyvas surikiuotas nuo trumpiausios pavardes iki ilgiausios
-for($i = 0; $i < sizeof($arrayOfPeople) + 1; $i++) {
-    $temp = '';
-    if ($i == sizeof($arrayOfPeople) - 1) {
-        break;
-    } 
-    if (strlen($arrayOfPeople[$i]) > strlen($arrayOfPeople[$i+1])) {
-       $temp = $arrayOfPeople[$i+1];
-       $arrayOfPeople[$i+1] = $arrayOfPeople[$i];
-       $arrayOfPeople[$i] = $temp;
-
-    }
+print '<h3>Pavardes nuo trumpiausios iki ilgiausios</h3>';
+function sortNames($a,$b){
+    return strlen($a) - strlen($b);
 }
 
+usort($arrayOfPeople,'sortNames');
 print_r($arrayOfPeople);
